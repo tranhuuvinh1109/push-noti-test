3a1fc20d48cf50d7dafcf1765c34d2ce76aeb41f
VinhTranHuu <tranhuuvinh1109@gmail.com>
2025-03-20 14:49:59 +0700

    update FE

---
+++ FE/script.js
@@ -0,0 +1,35 @@
+const checkPermission = () => {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error("No support for service worker!")
+    }
+
+    if (!('Notification' in window)) {
+        throw new Error("No support for notification API");
+    }
+
+    if (!('PushManager' in window)) {
+        throw new Error("No support for Push API")
+    }
+}
+
+const registerSW = async () => {
+    const registration = await navigator.serviceWorker.register('sw.js');
+    console.log('Service worker registration successful-------->', registration);
+    return registration;
+}
+
+const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission();
+
+    if (permission !== 'granted') {
+        throw new Error("Notification permission not granted")
+    }
+
+}
+
+const main = async () => {
+    checkPermission()
+    await requestNotificationPermission()
+    await registerSW()
+}
+
---
+++ FE/sw.js
@@ -0,0 +1,39 @@
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+}
+
+const saveSubscription = async (subscription) => {
+    const response = await fetch('https://push-noti-test.vercel.app/save-subscription', {
+        method: 'post',
+        headers: { 'Content-type': "application/json" },
+        body: JSON.stringify(subscription)
+    })
+
+    return response.json()
+}
+
+self.addEventListener("activate", async (e) => {
+    const subscription = await self.registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array("BKSDhnuXvFRQdLgp6LjUEyK67TdzWm5C61MX8vCKNsbAjsmdg-hob7-B1mp-qUdRZiOD7EJ4i_oKAJhgfUyesIs")
+    })
+
+    const response = await saveSubscription(subscription)
+    console.log(response)
+})
+
+self.addEventListener("push", e => {
+    self.registration.showNotification("Wohoo!!", { body: e.data.text() })
+})
